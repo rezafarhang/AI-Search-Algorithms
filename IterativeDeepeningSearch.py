681af7f153d3c6b933d98e837237b2f384a55a06
reza <rezafarhangoa@gmail.com>
2022-01-17 21:20:41 +0330

    add visited list to ignore visited nodes

--- IterativeDeepeningSearch.py
+++ IterativeDeepeningSearch.py
@@ -14,6 +14,8 @@ def path(start_node, node: GraphNode):
 cutoff = 90
 cutoff_occured = False
 
+visited = []
+
 
 def recursive_dls(node, start_node, end_node, limit):
     if node.state == end_node:
@@ -24,23 +26,25 @@ def recursive_dls(node, start_node, end_node, limit):
     else:
         cutoff_occurred = False
         # print(node.state[0], node.state[1], action(node.state[0], node.state[1]))
-        for ac in action(node.state[0], node.state[1]):
-            child_state = None
-            if ac == 'right':
-                child_state = (node.state[0], node.state[1] + 1)
-            elif ac == 'left':
-                child_state = (node.state[0], node.state[1] - 1)
-            elif ac == 'up':
-                child_state = (node.state[0] - 1, node.state[1])
-            elif ac == 'down':
-                child_state = (node.state[0] + 1, node.state[1])
+        if node.state not in visited:
+            visited.append(node.state)
+            for ac in action(node.state[0], node.state[1]):
+                child_state = None
+                if ac == 'right':
+                    child_state = (node.state[0], node.state[1] + 1)
+                elif ac == 'left':
+                    child_state = (node.state[0], node.state[1] - 1)
+                elif ac == 'up':
+                    child_state = (node.state[0] - 1, node.state[1])
+                elif ac == 'down':
+                    child_state = (node.state[0] + 1, node.state[1])
 
-            child_node = GraphNode(child_state, node, ac, node.cost + 1)
-            result = recursive_dls(child_node, start_node, end_node, limit - 1)
-            if result == cutoff:
-                cutoff_occurred = True
-            elif result != 0:
-                return result
+                child_node = GraphNode(child_state, node, ac, node.cost + 1)
+                result = recursive_dls(child_node, start_node, end_node, limit - 1)
+                if result == cutoff:
+                    cutoff_occurred = True
+                elif result != 0:
+                    return result
         if cutoff_occurred:
             return cutoff
         else:
@@ -53,17 +57,18 @@ def dls(start_node, end_node, limit):
 
 
 def ids(start_node, end_node, max_depth):
-    depth = 0
+    # depth = 0
     for depth in range(max_depth):
+        visited.clear()
         result = dls(start_node, end_node, depth)
         if result != cutoff:
             return result
-        depth = depth + 1
+        depth += 1
 
 
 output1 = ids((0, 0), (3, 6), 400)
 print(output1)
-output2 = ids((3, 6), (10, 12), 400)
+output2 = ids((3, 6), (15, 15), 400)
 print(output2)
 print(output2 + output1[1:])
 
