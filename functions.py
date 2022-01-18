9be965f0094be63235b9a841cb0f7be4eabd6e0f
reza <rezafarhangoa@gmail.com>
2022-01-18 19:36:10 +0330

    add is_goal and get_path functions

--- functions.py
+++ functions.py
@@ -22,3 +22,21 @@ def action(x, y):
         if binary_matrix[x][y - 1] == 1:
             possible.append("left")
     return possible
+
+
+def is_goal(node, end_node):
+    if node == end_node:
+        return 1
+    return 0
+
+
+def get_path(current_node):
+    return [current_node[1]] + current_node[2]
+
+
+def path(start_node, node):
+    path_list = [node.state]
+    while node.state != start_node:
+        path_list.append(node.parent.state)
+        node = node.parent
+    return path_list
