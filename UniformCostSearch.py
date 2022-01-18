a19cbefd62c892159150eaf9f7d14d264ece08ab
reza <rezafarhangoa@gmail.com>
2022-01-18 19:28:45 +0330

    stuck in to the loop bug fix

--- UniformCostSearch.py
+++ UniformCostSearch.py
@@ -1,44 +1,55 @@
-from GraphNode import GraphNode
-from Qu import Queue
-from functions import action, path
+from maze import binary_matrix
+import heapq
 
 
-def ucs(start_node, end_node):
-    root = GraphNode(start_node, None, None, 0)
-    frontier = Queue()
-    frontier.enqueue(root)
-    explored = []
+def is_goal(node, end_node):
+    if node == end_node:
+        return 1
+    return 0
+
+
+def get_path(current_node):
+    return [current_node[1]] + current_node[2]
+
+
+def UCS(start_node, end_node):
+    node_count = 0
+    frontier = [(0, start_node, list(), 0)]
+    heapq.heapify(frontier)
+    explored = {}
+    path = []
 
     while True:
-        if frontier.isempty():
-            return 'failure'
-        node = frontier.dequeue()
-        if node.state == end_node:
-            print(path(start_node, node))
-            return 1
-        explored.append(node.state)
-        for ac in action(node.state[0], node.state[1]):
-            child_state = None
-            if ac == 'right':
-                child_state = (node.state[0], node.state[1] + 1)
-            elif ac == 'left':
-                child_state = (node.state[0], node.state[1] - 1)
-            elif ac == 'up':
-                child_state = (node.state[0] - 1, node.state[1])
-            elif ac == 'down':
-                child_state = (node.state[0] + 1, node.state[1])
+        if len(frontier) < 1:
+            print('fail')
+            return 0
 
-            child_node = GraphNode(child_state, node, ac, node.cost + 1)
-            if child_node.state not in explored or frontier.state_exist(child_node.state):
-                frontier.enqueue(child_node)
-                frontier.queue.sort(key=lambda x: x.cost)
-            elif child_node.state in explored:
-                # for nod in frontier.queue:
-                #     if nod.state == child_node.state:
-                #         if node.cost > child_node.cost:
-                #             frontier.queue.remove(nod)
-                #             frontier.enqueue(child_node)
+        state = heapq.heappop(frontier)
+        if is_goal(state[1], end_node):
+            return get_path(state), node_count
+
+        explored[state[1]] = state[3]
+        neighbors = list()
+        (i, j) = state[1]
+        for neighbor in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            node_position = (i + neighbor[0], j + neighbor[1])
+            if node_position[0] < 19 and node_position[0] > -1 and node_position[1] > -1 and node_position[1] < 19:
+                if binary_matrix[node_position[0]][node_position[1]] > 0:
+                    neighbors.append(node_position)
+
+        for n in neighbors:
+            next_cost = state[3] + 1
+            if n in explored and explored[n] < next_cost:
+                continue
+            heapq.heappush(frontier, (next_cost, n, [state[1]] + state[2], next_cost))
+            node_count += 1
 
 
-ucs((0, 0), (3, 6))
-ucs((3, 6), (15, 15))
+(output1, node_count1) = UCS((0, 0), (3, 6))
+(output2, node_count2) = UCS((3, 6), (15, 15))
+print((output2 + output1[1:])[::-1])
+print("node_count: ", node_count2 + node_count1)
+
+# [(15, 15), (14, 15), (14, 14), (13, 14), (12, 14), (11, 14), (10, 14), (9, 14), (8, 14), (7, 14), (6, 14), (5, 14),
+# (5, 13), (4, 13), (3, 13), (3, 12), (3, 11), (3, 10), (3, 9), (3, 8), (3, 7), (3, 6), (2, 6), (1, 6), (0, 6), (0,
+# 5), (0, 4), (0, 3), (0, 2), (0, 1), (0, 0)]
